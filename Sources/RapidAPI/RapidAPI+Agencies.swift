3148c8b8c699c300a6d753b6fc4cc3589b066f56
Fritz Anderson <fritza@uchicago.edu>
2021-06-14 15:02:51 -0500

    Add RapidAPI+Agencies (old version of RapidAPI had it in RapidAPI.swift)
    Also RapidAPI.Errors
    TODO: Repair RapidAPI Swift to account for the differrences.

---
+++ Sources/RapidAPI/RapidAPI+Agencies.swift
@@ -0,0 +1,83 @@
+//
+//  RapidAPI+Agencies.swift
+//  UCMaps
+//
+//  Created by Fritz Anderson on 6/2/21.
+//  Copyright © 2021 The University of Chicago. All rights reserved.
+//
+
+import Foundation
+import RapidAPI
+
+extension RapidAPI {
+    /// An `OptionSet` selecting the transit agencies the application supports.
+    ///
+    /// These are hard-coded to UChicago (`.uchicago`) and CTA (`.cta`). There are allso `.all` and `.none` sets.
+    public struct Agencies: OptionSet {
+        // FIXME: Reconcile with the hand-initialization in APIAgencies.swift
+        public let rawValue: Int
+        public init(rawValue: Int) { self.rawValue = rawValue }
+
+        static let cta = Agencies(rawValue: 1)
+        static let uchicago = Agencies(rawValue: 2)
+        static let all: Agencies  = [.cta, .uchicago]
+        static let none: Agencies = []
+
+        public var onlyOneAgency: Bool {
+            self.rawValue != Agencies.none.rawValue &&
+                self.rawValue != Agencies.all.rawValue
+        }
+        public func preconditionOnlyOne(
+            file: String = #file,
+            line: Int = #line,
+            function: String = #function) {
+            precondition(onlyOneAgency,
+                         """
+\(file):\(line) - \(function)\n\tID (\(rawValue)) isn't a single agency
+"""
+            )
+        }
+
+        public var ID: String? {
+            preconditionOnlyOne()
+            switch self {
+            case .cta:      return "104"
+            case .uchicago: return "100"
+            default:        return nil
+            }
+        }
+
+        public var IDs: [String] {
+            var items: [String] = []
+            for s: Agencies in [.cta, .uchicago] where self.contains(s) {
+                items.append(s.ID!)
+            }
+            return items
+        }
+
+        public var name: String? {
+            preconditionOnlyOne()
+            switch self {
+            case .cta:      return "CTA"
+            case .uchicago: return "UChicago"
+            default:        return nil
+            }
+        }
+
+        public var names: [String] {
+            var items: [String] = []
+            for s: Agencies in [.cta, .uchicago] where self.contains(s) {
+                items.append(s.name!)
+            }
+            return items
+        }
+
+        public var number: String {
+            return IDs.joined(separator: ",")
+        }
+
+        public var fragment: String {
+            "agencies=\(number)"
+        }
+    }
+}
---
+++ Sources/RapidAPI/RapidAPI+Errors.swift
@@ -0,0 +1,55 @@
+//
+//  RapidAPI+Errors.swift
+//  UCMaps
+//
+//  Created by Fritz Anderson on 6/2/21.
+//  Copyright © 2021 The University of Chicago. All rights reserved.
+//
+
+import Foundation
+import RapidAPI
+
+extension RapidAPI {
+
+    /// Errors arising from the `RapidAPI` functions.
+    ///
+    /// - `badURL`: The `url(verb:agencies:routes:)` public func could not form a valid URL
+    /// - `mustSpecifyAgency`: `agencies` is `.none`.
+    /// - `mustNotSpecifyRoutes`: `routes` were provided when the query does not support them.
+    public enum Errors: Error {
+        case badURL(String)
+        case mustSpecifyAgency(Verbs)
+        case mustNotSpecifyRoutes(Verbs)
+        case queryFailed(URL)
+
+        case reentrantUnitChain
+        case startingEmptyChain
+        case noManagedObjectContext(String)
+        case taskInsertionWhileExecuting(String)
+
+        public var localizedDescription: String {
+            switch self {
+            case .badURL(let url):
+                return "Generated URL is bad: \(url)"
+            case .mustSpecifyAgency(let verb):
+                return "Agency must be specified for \(verb.rawValue)"
+            case .mustNotSpecifyRoutes(let verb):
+                return "\(verb.rawValue) must not specify routes."
+            case .queryFailed(let url):
+                return "Could not get a response to “\(url.absoluteString)”"
+
+            case .reentrantUnitChain:
+                return "Attempt to re-enter the public initial unit chain"
+            case .startingEmptyChain:
+                return "Attempt to start an empty unit chain"
+            case .noManagedObjectContext(let name):
+                return "Attempt to execute \(name) without the optional NSManagedObjectContext"
+            case .taskInsertionWhileExecuting(let name):
+                return "Attempt to add \(name) while units are executing"
+
+
+            }
+
+        }
+    }
+}
