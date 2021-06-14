8a20751a4dba88d0fd6a2141b0205fdcc92849a3
Fritz Anderson <fritza@uchicago.edu>
2021-06-14 13:59:39 -0500

    Mark lots of types and properties public in enum RapidAPI public
    Remove the definition of "TransLocWrapper," which should not be in use. I think.

--- Sources/RapidAPI/RapidAPI.swift
+++ Sources/RapidAPI/RapidAPI.swift
@@ -8,12 +8,13 @@
 
 import Foundation
 
+public 
 enum RapidAPI: String {
     case host = "transloc-api-1-2.p.rapidapi.com"
     case apiKey = "624c197464msh7f14d937c6272dbp1767b3jsnba315478bdc8"
     
     /// The queries TransLoc's OpenAPI 1.2 supports
-    enum Verbs: String {
+    public enum Verbs: String {
         case agencies, routes, segments, vehicles, stops
         case arrivalEstimates = "arrival-estimates"
         
@@ -27,7 +28,7 @@ enum RapidAPI: String {
         //, .stops, .arrivalEstimates]
         var isRouteQualifiable: Bool { Self.routeQualifiable.contains(self) }
         
-        func request(agencies: Agencies,
+        public func request(agencies: Agencies,
                      routes: [String]? = nil)
             throws -> URLRequest {
                 let url = try self.url(agencies: agencies, routes: routes)
@@ -36,7 +37,7 @@ enum RapidAPI: String {
                 return retval
         }
         
-        func url(agencies: Agencies, routes: [String]? = nil) throws -> URL {
+        public func url(agencies: Agencies, routes: [String]? = nil) throws -> URL {
             guard !agencies.isEmpty else { throw Errors.mustSpecifyAgency(self) }
             var fragments: [String] = [agencies.fragment]
             
@@ -62,16 +63,16 @@ enum RapidAPI: String {
     /// The transit agencies the application supports.
     ///
     /// These are hard-coded to UChicago (`.uchicago`) and CTA (`.cta`). There are allso `.all` and `.none` sets.
-    struct Agencies: OptionSet {
-        let rawValue: Int
-        init(rawValue: Int) { self.rawValue = rawValue }
+    public struct Agencies: OptionSet {
+        public let rawValue: Int
+        public init(rawValue: Int) { self.rawValue = rawValue }
         
         static let cta = Agencies(rawValue: 1)
         static let uchicago = Agencies(rawValue: 2)
         static let all: Agencies  = [.cta, .uchicago]
         static let none: Agencies = []
         
-        var number: String {
+        public var number: String {
             var items: [String] = []
             if self.contains(.cta) {
                 items.append("104")
@@ -82,7 +83,7 @@ enum RapidAPI: String {
             return items.joined(separator: ",")
         }
         
-        var fragment: String {
+        public var fragment: String {
             "agencies=\(number)"
         }
     }
@@ -92,7 +93,7 @@ enum RapidAPI: String {
     /// - `badURL`: The `url(verb:agencies:routes:)` func could not form a valid URL
     /// - `mustSpecifyAgency`: `agencies` is `.none`.
     /// - `mustNotSpecifyRoutes`: `routes` were provided when the query does not support them.
-    enum Errors: Error {
+    public enum Errors: Error {
         case badURL(String)
         case mustSpecifyAgency(Verbs)
         case mustNotSpecifyRoutes(Verbs)
@@ -136,7 +137,7 @@ enum RapidAPI: String {
     ///   - routes: The routes to filter down to. Defaults to  `nil`. Only `.segments` and `.vehicles` may specify routes.
     /// - Throws: `.mustSpecifyAgency` if `agencies` is `.none`; `.mustNotSpecifyRoutes` if the `verb` does not support route filtering; and `.badURL` if no URL could be built from the query information (this should be a fatal error).
     /// - Returns: A `URL` representing the desired query.
-    static func url(verb: Verbs,
+    public static func url(verb: Verbs,
                     agencies: Agencies,
                     routes: [String]? = nil)
         throws -> URL
@@ -172,56 +173,4 @@ enum RapidAPI: String {
         retval.dateDecodingStrategy = .iso8601
         return retval
     }()
-    
-    
-//
-//    /// A `URLRequest` to execute a TransLoc OpenAPI 1.2 query.
-//    /// - Parameters:
-//    ///   - verb: The query operation, as specified in `enum Verbs`
-//    ///   - agencies: The agencies to filter down to. These must not be `.none `.
-//    ///   - routes: The routes to filter down to. Defaults to  `nil`. Only `.segments` and `.vehicles` may specify routes.
-//    /// - Throws: `.mustSpecifyAgency` if `agencies` is `.none`; `.mustNotSpecifyRoutes` if the `verb` does not support route filtering; and `.badURL` if no URL could be built from the query information (this should be a fatal error).
-//    /// - Returns: A `URLRequest` representing the desired query.
-//    /// - note: The Alamofire `Request` API should largely supersede this.
-//    static func request(verb: Verbs,
-//                        agencies: Agencies,
-//                        routes: [String]? = nil)
-//        throws -> URLRequest {
-//            let url = try Self.url(verb: verb,
-//                                   agencies: agencies,
-//                                   routes: routes)
-//            var retval = URLRequest(url: url,
-//                                    cachePolicy: .useProtocolCachePolicy,
-//                                    timeoutInterval: 10)
-//            retval.allHTTPHeaderFields = headers
-//            return retval
-//    }
-}
-
-/// The container JSON for the per-API response.
-///
-/// `JSONDecoder` for an API result should specify the payload type in the generic parameter:
-///
-///     let allRoutes =
-///         try jsonDecoder.decode(TransLocWrapper<[String:[TranslocRoute]]>.self,
-///                                from: routeData)
-/// The payload structure willl then be in `allRoutes.data`.
-struct TransLocWrapper<T:Decodable>: Decodable, CustomStringConvertible {
-    let rate_limit: Int
-    let expires_in: Double
-    let api_latest_version: String
-    let api_version: String
-    let generated_on: Date
-    let data: T
-    
-    var description: String {
-        var retval = "TransLocWrapper:\n"
-        print("\tgenerated:", generated_on, "expires in", expires_in,
-              "sec  Rate limit:", rate_limit, to: &retval)
-        print("\tAPI version:", api_version,
-              "latest:", api_latest_version, to: &retval)
-        return retval
-    }
-    
-    var payload: T { self.data }
 }
